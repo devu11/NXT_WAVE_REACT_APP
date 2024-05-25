2419e71c003376895add6ea3ff09c6a88f7d4898
devu11 <devudevzz3@gmail.com>
2024-05-25 13:11:06 +0530

    added navbar and booksection

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <div className="navbar-container" >
+      <nav className="navbar ">
+        <div className="navbar-logo">
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSULYExsUDfGLx0wtDYblGzdIpzWkYlgbAkAbsrC-r5WyBoDpKS1UjVt1oRkP0Ss1Govng&usqp=CAU"
+            alt="Company Logo"
+          />
+        </div>
+        <div className="navbar-actions">
+          <button className="add-button">ADD ITEM</button>
+          <div className="profile-circle">
+            <img src="https://i.pinimg.com/736x/68/c5/0a/68c50ac30605fdb8ce0836aaeaacd48c.jpg" alt="Profile" />
+          </div>
+        </div>
+      </nav>
+      <hr className="navbar-line" />
+    </div>
+  );
+}
+
+export default Navbar;
---
+++ src/components/SectionBox/SectionBox.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './SectionBox.css'
+
+function SectionBox() {
+  return (
+    <div className="section-box">
+      <div className="section resources-section">
+        <h3>Resources</h3>
+      </div>
+      <div className="section">
+        <h3>Request</h3>
+      </div>
+      <div className="section">
+        <h3>User</h3>
+      </div>
+    </div>
+  )
+}
+
+export default SectionBox
